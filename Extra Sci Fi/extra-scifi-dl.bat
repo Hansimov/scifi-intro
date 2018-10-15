5d0fdd4fdef8a8f43089d44e45e907203f516d06
Hansimov <591172499@qq.com>
2018-10-15 23:16:00 +0800

    + Modify file structure + Add Extra Sci Fi 01 English subtitle

---
+++ Extra Sci Fi/extra-scifi-dl.bat
@@ -0,0 +1,12 @@
+@echo off
+rem youtube-dl -v ePzdchSJI8I --proxy "socks5://127.0.0.1/"
+rem youtube-dl --list-subs ePzdchSJI8I --proxy "socks5://127.0.0.1/"
+rem youtube-dl --all-subs --skip-download ePzdchSJI8I --proxy "socks5://127.0.0.1/"
+rem youtube-dl --write-sub --sub-format en --skip-download ePzdchSJI8I --proxy "socks5://127.0.0.1/"
+rem youtube-dl --list-formats ePzdchSJI8I
+rem youtube-dl -i -f mp4 --yes-playlist PL08jItIqOb2qyMOhtEUoLh100KpccQiRf
+youtube-dl -f mp4 --write-sub --sub-format en --yes-playlist PLhyKYa0YJ_5AuEhpcGAo4ngmSDKuFgZZx
+rem youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 --write-sub --sub-format en --yes-playlist PLhyKYa0YJ_5AuEhpcGAo4ngmSDKuFgZZx
+rem youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' jd9Baab9Xsc
+rem youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' jd9Baab9Xsc
+rem youtube-dl --write-sub --sub-format en --skip-download jd9Baab9Xsc
